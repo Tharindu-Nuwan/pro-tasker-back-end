28f9bd4d4f455ae1ae0e24a5ff8e5b38e34b4c5a
Tharindu Nuwan Madhushanka <tharindunm94@gmail.com>
2023-12-30 21:19:25 +0530

    Implement getAll and createNewTask methods in taskHttpController

--- src/main/java/lk/ijse/dep11/app/WebAppConfig.java
+++ src/main/java/lk/ijse/dep11/app/WebAppConfig.java
@@ -2,10 +2,12 @@ package lk.ijse.dep11.app;
 
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @ComponentScan
 @EnableWebMvc
+@PropertySource("classpath:/application.properties")
 public class WebAppConfig {
 }
---
+++ src/main/java/lk/ijse/dep11/app/api/TaskHttpController.java
@@ -0,0 +1,78 @@
+package lk.ijse.dep11.app.api;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import lk.ijse.dep11.app.to.TaskTO;
+import org.springframework.core.env.Environment;
+import org.springframework.http.HttpStatus;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.PreDestroy;
+import java.sql.*;
+import java.util.LinkedList;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/tasks")
+@CrossOrigin
+public class TaskHttpController {
+    private final HikariDataSource pool;
+
+    public TaskHttpController(Environment env) {
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(env.getRequiredProperty("app.datasource.url"));
+        config.setUsername(env.getRequiredProperty("app.datasource.username"));
+        config.setPassword(env.getRequiredProperty("app.datasource.password"));
+        config.setDriverClassName(env.getRequiredProperty("app.datasource.driver-class-name"));
+        config.setMaximumPoolSize
+                (env.getRequiredProperty("app.datasource.hikari.maximum-pool-size", Integer.class));
+        pool = new HikariDataSource(config);
+    }
+
+    @PreDestroy
+    private void destroy() {
+        pool.close();
+    }
+
+    @ResponseStatus(HttpStatus.CREATED)
+    @PostMapping(produces = "application/json", consumes = "application/json")
+    public TaskTO createTask(@RequestBody @Validated(TaskTO.Create.class) TaskTO task) {
+        try(Connection connection = pool.getConnection()) {
+            PreparedStatement stm = connection
+                    .prepareStatement("INSERT INTO task (description, status, email) VALUES (?, FALSE, ?)",
+                            Statement.RETURN_GENERATED_KEYS);
+            stm.setString(1 ,task.getDescription());
+            stm.setString(2 ,task.getEmail());
+            stm.executeUpdate();
+            ResultSet generatedKeys = stm.getGeneratedKeys();
+            generatedKeys.next();
+            int id = generatedKeys.getInt(1);
+            task.setId(id);
+            task.setStatus(false);
+            return task;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @GetMapping(produces = "application/json", params = {"email"})
+    public List<TaskTO> getAllTasks(String email) {
+        try (Connection connection = pool.getConnection()) {
+            PreparedStatement stm = connection
+                    .prepareStatement("SELECT * FROM task WHERE email = ? ORDER BY id");
+            stm.setString(1, email);
+            ResultSet rst = stm.executeQuery();
+            List<TaskTO> taskList = new LinkedList<>();
+            while (rst.next()) {
+                int id = rst.getInt("id");
+                String description = rst.getString("description");
+                boolean status = rst.getBoolean("status");
+                taskList.add(new TaskTO(id, description, status, email));
+            }
+            return taskList;
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
